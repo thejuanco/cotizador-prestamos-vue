13b8983e04e827f90c734313bf4eb2bf2a4a4fc6
Juanco <juanffo874@gmail.com>
2024-12-17 15:42:39 -0600

    Crea componentes para los botones y sus props para leer los datos

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import { ref, reactive, computed } from 'vue';
   import Header from './components/Header.vue';
+  import Button from './components/Button.vue';
 
   const cantidad = ref(10000)
   // const state = reactive({
@@ -53,12 +54,14 @@
     <Header/>
 
     <div class="flex justify-between mt-10">
-      <button class="h-10 w-10 flex items-center justify-center font-bold bg-indigo-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-indigo-500 text-white text-2xl"
-        @click="handleChangeDecremento"
-      >-</button>
-      <button class="h-10 w-10 flex items-center justify-center font-bold bg-indigo-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-indigo-500 text-white text-2xl"
-        @click="handleChangeIncremento"
-      >+</button>
+      <Button
+        :operador="'-'"
+        :fn="handleChangeDecremento"
+      />
+      <Button
+        :operador="'+'"
+        :fn="handleChangeIncremento"
+      />
     </div>
 
     <div class="my-5">
---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<script setup>
+//Recuperar los props
+const props = defineProps({
+    operador: String,
+    fn: Function
+})
+</script>
+<template>
+    <button 
+        type="button"
+        class="h-10 w-10 flex items-center justify-center font-bold bg-indigo-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-indigo-500 text-white text-2xl"
+        @click="fn"
+    >{{operador}}</button>
+</template>
