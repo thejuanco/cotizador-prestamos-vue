f85bdb6e9f05e169c891a763c91e108aaa067efd
Juanco <juanffo874@gmail.com>
2024-12-17 16:03:13 -0600

    Crea la seccion de resumen de pagos

--- src/App.vue
+++ src/App.vue
@@ -90,6 +90,16 @@
         <option value="12">12 meses</option>
         <option value="24">24 meses</option>
       </select>
+
+      <div class="my-5 space-y-3 bg-gray-100 rounded-lg p-5">
+        <h2 class="text-2xl font-extrabold text-gray-500 text-center">
+          Resumen <span class="text-indigo-600">de pagos</span>
+        </h2>
+
+        <p class="text-xl text-gray-500 text-center font-bold">{{meses}} Meses</p>
+        <p class="text-xl text-gray-500 text-center font-bold">Total a pagar:</p>
+        <p class="text-xl text-gray-500 text-center font-bold">Mensuales:</p>
+      </div>
     </div>
   </div>
 </template>
